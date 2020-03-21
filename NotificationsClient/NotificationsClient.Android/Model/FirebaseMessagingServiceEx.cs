3ee438b61e52eb61018f4847ce4422037887c2c4
lbugnion <laurent@galasoft.ch>
2020-03-21 12:53:36 +0100

    Testing with Azure Notifications Hub, receiving test notification works

---
+++ NotificationsClient/NotificationsClient.Android/Model/FirebaseMessagingServiceEx.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.Content;
+using Firebase.Messaging;
+using System;
+using WindowsAzure.Messaging;
+
+namespace NotificationsClient.Droid.Model
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseMessagingServiceEx : FirebaseMessagingService
+    {
+        private const string NotificationHubName = "LbNotifications";
+        private const string ConnectionString = "Endpoint=sb://lbnotifications.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=AKc4JqFZq8hDvwpdF0CZzFCEBzdsWTkt3xUSSgQ1DYo=";
+        private const string TagName = "default";
+        private const string TemplateName = "defaultTemplate";
+        private const string Template = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+            SendRegistrationToServer(token);
+        }
+
+        public override void OnMessageReceived(RemoteMessage remoteMessage)
+        {
+            base.OnMessageReceived(remoteMessage);
+
+            var message = string.Empty;
+
+            foreach (var key in remoteMessage.Data.Keys)
+            {
+                var value = remoteMessage.Data[key];
+                message += $"{key}:{value} |";
+            }
+
+            GalaSoft.MvvmLight.Messaging.Messenger.Default.Send(message);
+        }
+
+        private void SendRegistrationToServer(string token)
+        {
+            try
+            {
+                var hub = new NotificationHub(NotificationHubName, ConnectionString, this);
+
+                // register device with Azure Notification Hub using the token from FCM
+                var registration = hub.Register(token, TagName);
+
+                // subscribe to the SubscriptionTags list with a simple template.
+                var pnsHandle = registration.PNSHandle;
+                var templateReg = hub.RegisterTemplate(
+                    pnsHandle, 
+                    TemplateName, 
+                    Template,
+                    TagName);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
